28e78145a44b7d381613519445072b203216f990
Vladimir <vkorotkih.4work@gmail.com>
2023-07-02 21:28:58 +0200

    Рабочий калькулятор

--- WpfTestApp/MainWindow.xaml.cs
+++ WpfTestApp/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,6 +24,50 @@ namespace WpfTestApp
         public MainWindow()
         {
             InitializeComponent();
+
+            foreach (UIElement el in MainGrid.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button) el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button) e.OriginalSource).Content;
+
+            //if (str == "AC")
+            //    MainTextLabel.Text = "";
+            //else
+            //    MainTextLabel.Text += str;
+
+            switch (str)
+            {
+                case "AC":
+                    MainTextLabel.Text = "";
+                    break;
+                case "=":
+                    string value = new DataTable().Compute(MainTextLabel.Text, null).ToString();
+                    MainTextLabel.Text = value;
+                    break;
+                //case "+":
+                //    MainTextLabel.Text = "PLUS";
+                //    break;
+                //case "*":
+                //    MainTextLabel.Text = "UMNOZHIT";
+                //    break;
+                //case "/":
+                //    MainTextLabel.Text = "DELIM";
+                //    break;
+                //case "-":
+                //    MainTextLabel.Text = "MINUS";
+                //    break;
+                default:
+                    MainTextLabel.Text += str;
+                    break;
+            }
         }
     }
 }
